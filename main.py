79eb4870865970cb96d0bd0bf9dad70b8645bc99
fillipimangrich <costa.mangrich@gmail.com>
2023-04-28 20:40:02 -0300

    pronto

--- main.py
+++ main.py
@@ -3,21 +3,31 @@ from BuscaEmLargura import BuscaEmLargura
 from CicloEuleriano import CicloEuleriano
 from Dijkstra import Dijkstra
 from FloydWarshall import FloydWarshall
-grafo = Grafo()
+##########################################################################
+# =======================================================================#
+# PARA RODAR O CODIGO BASTA DESCOMENTAR A SECAO CORRESPONDENTE A QUESTAO #
+# =======================================================================#
+##########################################################################
 
-# ======================================
+
 # questao 1
-#grafo.ler("tests/facebook_santiago.net")
+# ======================================
 
-#for key in grafo.vertices.keys():
+# grafo = Grafo()
+# grafo.ler("tests/facebook_santiago.net")
+
+# for key in grafo.vertices.keys():
 #    print(f"{key}: {grafo.vertices[key].indice} - {grafo.vertices[key].rotulo}")
 
 # for key in grafo.arestas.keys():
 #     print(f"{key}: {grafo.arestas[key].u.indice}, {grafo.arestas[key].v.indice}  - {grafo.arestas[key].peso}")
-# ======================================
 
 # ======================================
+
+
 # questao 2
+# ======================================
+
 # grafo2 = Grafo()
 # grafo2.ler("tests/adjnoun.net")
 # indice_vertice = int(input("Digite o indice do vertice s:"))
@@ -26,17 +36,22 @@ grafo = Grafo()
 # busca.run()
 # ======================================
 
-# ======================================
+
 # questao 3
+# ======================================
+
 # grafo3 = Grafo()
 # grafo3.ler("tests/test.net")
 
-#ciclo = CicloEuleriano(grafo2)
-#ciclo.printEulerian()
-# ======================================
+# ciclo = CicloEuleriano(grafo3)
+# ciclo.printEulerian()
 
 # ======================================
+
+
 # questao 4
+# ======================================
+
 # grafo4 = Grafo()
 # grafo4.ler("tests/test.net")
 # grafo4.ler("tests/adjnoun.net")
