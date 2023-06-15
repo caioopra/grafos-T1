13076f25d7dddaa374aa56939dd4bddfa1f73e44
Caio <caioprsilva@gmail.com>
2023-06-15 16:03:38 -0300

    Implementação de Edmonds Karp

--- atividade3/EdmondsKarp.py
+++ atividade3/EdmondsKarp.py
@@ -1,32 +1,69 @@
+from collections import (
+    deque,
+)  # https://docs.python.org/3/library/collections.html#collections.deque
 from Grafo.Grafo import Grafo
-from Grafo.Vertice import Vertice
 
 
 class EdmondsKarp:
-    def __init__(self, grafo: Grafo, origem: int, sorvedouro: int, rede: Grafo):
+    def __init__(self, grafo: Grafo, inicial: int, final: int):
         self.__grafo = grafo
-        self.__origem = origem
-        self.__sorvedouro = sorvedouro
-        self.__rede = rede
-        
-        self.__C = {}
-        self.__A = {}
-        self.__queue = []
-    
-    def initialize(self):
-        for i in range(self.__grafo.qtdVertices() + 1):
-            self.__C[i] = False
-            self.__A[i] = None
-        self.__C[self.__sorvedouro] = True
-        self.__queue.append(self.__grafo.vertices[self.__sorvedouro])  # armazenando objetos Vertice
-        
-    def search(self):
-        while not len(self.__queue):
-            u = self.__queue.pop()
-            
-            for vizinho in u.vizinhos_saintes():
-                if not self.__C[vizinho.indice] and 
-    
+        self.__inicial = inicial
+        self.__final = final
+
+        self.__fluxo = -0
+        self.__F = [
+            [0 for _ in range(self.__grafo.qtdVertices() + 1)]
+            for _ in range(self.__grafo.qtdVertices() + 1)
+        ]
+
     def run(self):
-        self.initialize()
-        self.search()
+        while True:
+            caminhoAumentante = [-1 for _ in range(self.__grafo.qtdVertices() + 1)]
+            caminhoAumentante[self.__inicial] = -1
+
+            capacidadeResidual = [0 for _ in range(self.__grafo.qtdVertices() + 1)]
+            capacidadeResidual[self.__inicial] = float("inf")
+            
+            self.__fila = deque([self.__inicial])
+
+            fluxoCaminho, caminhoAumentante = self.BFS(
+                caminhoAumentante, capacidadeResidual
+            )
+            
+            if fluxoCaminho == 0:
+                break
+        
+            self.__fluxo += fluxoCaminho
+            verticeAtual = self.__final
+            
+            while verticeAtual != self.__inicial:
+                vizinho = caminhoAumentante[verticeAtual]
+                self.__F[vizinho][verticeAtual] += fluxoCaminho
+                self.__F[verticeAtual][vizinho] -= fluxoCaminho
+                verticeAtual = vizinho
+    
+        print(f"Fluxo máximo {self.__inicial} -> {self.__final}: {self.__fluxo}")
+
+    def BFS(self, caminhoAumentante, capacidadeResidual):
+        while len(self.__fila) > 0:
+            verticeAtual = self.__fila.popleft()
+
+            for vizinho in self.__grafo.vizinhos_saintes(verticeAtual):
+                residual = (
+                    self.__grafo.peso(verticeAtual, vizinho.indice)
+                    - self.__F[verticeAtual][vizinho.indice]
+                )
+                
+                if residual > 0 and caminhoAumentante[vizinho.indice] == -1:
+                    caminhoAumentante[vizinho.indice] = verticeAtual
+                    capacidadeResidual[vizinho.indice] = min(
+                        capacidadeResidual[verticeAtual],
+                        residual
+                    )
+                    
+                    if vizinho.indice is not self.__final:
+                        self.__fila.append(vizinho.indice)
+                    else:
+                        return capacidadeResidual[self.__final], caminhoAumentante
+        
+        return 0, caminhoAumentante
--- main_entrega3.py
+++ main_entrega3.py
@@ -1,12 +1,40 @@
+from Grafo.Grafo import Grafo
 from atividade3.ColoracaoDeVertices import ColoracaoDeVertices
+from atividade3.EdmondsKarp import EdmondsKarp
+
+def test_1_edmonds_karp():
+    testes = {
+        1: {
+            "grafo": "wiki.net",
+            "entrada": [1, 5]  # [init, ent]
+        },
+        2: {
+            "grafo": "fluxo_m2.net",
+            "entrada": [2, 6]
+        }
+    }    
+    current_test = testes[2]
+    file_name = current_test['grafo']
+    init, end = current_test["entrada"]
+    
+    grafo = Grafo()
+    grafo.ler(f"tests/dirigidos/{file_name}")
+    
+    edmonds_karp = EdmondsKarp(grafo, init, end)
+    edmonds_karp.run()
+
+def test_2_hopcroft_karp():
+    ...
+
 
 def test_3_coloracaoDeVertices():
-    coloracao = ColoracaoDeVertices('tests/cor3.net')
+    coloracao = ColoracaoDeVertices("tests/cor3.net")
     coloracao.run()
 
 
 def main():
-    test_3_coloracaoDeVertices()
+    test_1_edmonds_karp()
+    # test_3_coloracaoDeVertices()
 
 
 if __name__ == "__main__":
